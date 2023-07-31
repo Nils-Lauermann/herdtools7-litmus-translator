28bec611f8a7f6ac779537d6ec1e919e1f3cc1cb
Ben Stokes <bcs42@cam.ac.uk>
2023-07-31 11:42:01 +0100

    some hacking to fill in gaps; now works on at least one test

--- herd/islaConverter.ml
+++ herd/islaConverter.ml
@@ -2,6 +2,8 @@
 
 open Printf
 
+module StringSet = Set.Make(String)
+
 module Make
     (A:Arch_herd.S) =
   struct
@@ -12,25 +14,28 @@ module Make
 
     let quote s = sprintf "\"%s\"" (String.escaped s)
 
-    let print_header (test : T.result) =
+    let print_header (test : T.result) (addresses : StringSet.t) =
       print_key "arch" (quote (Archs.pp A.arch));
       print_key "name" (quote test.Test_herd.name.Name.name);
       List.iter (fun (k, v) -> print_key (String.lowercase_ascii k) (quote v)) test.Test_herd.info;
-      print_endline "addresses = TODO"
+      (* isla-axiomatic docs say addresses can be used as a synonym for symbolic, but this doesn't seem to actually work, so symbolic it is *)
+      print_key "symbolic" (sprintf "[%s]" (String.concat ", " (List.map quote (StringSet.elements addresses))))
 
-    let process_init_state (test : T.result) : string list * string list IntMap.t =
+    let process_init_state (test : T.result) : StringSet.t * string list * string list IntMap.t =
       let update_cons x = function
         | None -> Some [x]
         | Some xs -> Some (x::xs) in
-      let accum loc v (locations, inits) = match loc with
+      let addresses = ref StringSet.empty in
+      let accum loc v (locations, inits) = (match v with | A.I.V.Var x -> printf "NOTE2: Var %s\n" (A.I.V.pp_csym x) | A.I.V.Val (Constant.Symbolic s) -> addresses := StringSet.add (Constant.pp_symbol s) !addresses | _ -> ()); match loc with
         | A.Location_reg (proc, reg) ->
-           let initialiser = key_value_str (A.pp_reg reg) (A.I.V.pp_v v) in
+           let initialiser = key_value_str (A.pp_reg reg) (quote (A.I.V.pp_v v)) in
            (locations, IntMap.update proc (update_cons initialiser) inits)
         | A.Location_global v2 ->
-           (* (match v2 with | Var x -> () | Val y -> ()); *)
+           (match v with | A.I.V.Var x -> printf "NOTE1: Var %s\n" (A.I.V.pp_csym x) | A.I.V.Val (Constant.Symbolic s) -> addresses := StringSet.add (Constant.pp_symbol s) !addresses | _ -> ());
            let initialiser = key_value_str (quote (A.I.V.pp_v v2)) (quote (A.I.V.pp_v v)) in
            (locations @ [initialiser], inits) in
-      A.state_fold accum test.Test_herd.init_state ([], IntMap.empty)
+      let (locs, inits) = A.state_fold accum test.Test_herd.init_state ([], IntMap.empty)
+      in (!addresses, locs, inits)
 
     let print_threads (test : T.result) (inits : string list IntMap.t) =
       let print_thread (proc, code, _) =
@@ -54,20 +59,22 @@ module Make
       | Or exprs -> String.concat " | " (List.map (fun expr -> bracket (format_constraint_expr expr)) exprs)
       | Implies (l, r) -> sprintf "(%s) -> (%s)" (format_constraint_expr l) (format_constraint_expr r)
 
-    let tmp_expand = let open ConstrGen in function
-    | ForallStates x -> x
-    | ExistsState y -> y
-    | NotExistsState z -> z
+    let expect_and_expr = let open ConstrGen in function
+    | ForallStates e -> ("unsat", Not e)
+    | ExistsState e -> ("sat", e)
+    | NotExistsState e -> ("unsat", e)
 
     let print_converted (test : T.result) =
-      print_header test;
+      let (addresses, locations, inits) = process_init_state test in
+      print_header test addresses;
       print_newline ();
       print_endline "[locations]";
-      let (locations, inits) = process_init_state test in
       List.iter print_endline locations;
       print_threads test inits;
       print_newline ();
       print_endline "[final]";
       (* print_endline "TODO"; *)
-      print_key "assertion" (quote (format_constraint_expr (tmp_expand test.Test_herd.cond)))
+      let (expect, expr) = expect_and_expr test.Test_herd.cond in
+      print_key "expect" (sprintf "\"%s\"" expect);
+      print_key "assertion" (quote (format_constraint_expr expr))
   end
