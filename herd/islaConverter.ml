974688f428d4511bca1586f5e9167e3267297f38
Ben Stokes <bcs42@cam.ac.uk>
2023-07-31 15:03:21 +0100

    clean up a bit

--- herd/islaConverter.ml
+++ herd/islaConverter.ml
@@ -1,6 +1,5 @@
-(* TODO mli file *)
-
 open Printf
+open Test_herd
 
 module StringSet = Set.Make(String)
 
@@ -16,25 +15,29 @@ module Make
 
     let print_header (test : T.result) (addresses : StringSet.t) =
       print_key "arch" (quote (Archs.pp A.arch));
-      print_key "name" (quote test.Test_herd.name.Name.name);
-      List.iter (fun (k, v) -> print_key (String.lowercase_ascii k) (quote v)) test.Test_herd.info;
+      print_key "name" (quote test.name.Name.name);
+      List.iter (fun (k, v) -> print_key (String.lowercase_ascii k) (quote v)) test.info;
       (* isla-axiomatic docs say addresses can be used as a synonym for symbolic, but this doesn't seem to actually work, so symbolic it is *)
-      print_key "symbolic" (sprintf "[%s]" (String.concat ", " (List.map quote (StringSet.elements addresses))))
+      print_key "symbolic" (addresses |> StringSet.elements |> List.map quote |> String.concat ", " |> sprintf "[%s]")
 
     let process_init_state (test : T.result) : StringSet.t * string list * string list IntMap.t =
       let update_cons x = function
         | None -> Some [x]
         | Some xs -> Some (x::xs) in
       let addresses = ref StringSet.empty in
-      let accum loc v (locations, inits) = (match v with | A.I.V.Var x -> printf "NOTE2: Var %s\n" (A.I.V.pp_csym x) | A.I.V.Val (Constant.Symbolic s) -> addresses := StringSet.add (Constant.pp_symbol s) !addresses | _ -> ()); match loc with
+      let process_v = function
+        | A.I.V.Var i -> printf "Encountered Var: %s\n" (A.I.V.pp_csym i) (* not sure what this is, doesn't trigger anywhere in the tests from HAND *)
+        | A.I.V.Val (Constant.Symbolic s) -> addresses := StringSet.add (Constant.pp_symbol s) !addresses
+        | _ -> () in
+      let accum loc v (locations, inits) = process_v v; match loc with
         | A.Location_reg (proc, reg) ->
            let initialiser = key_value_str (A.pp_reg reg) (quote (A.I.V.pp_v v)) in
            (locations, IntMap.update proc (update_cons initialiser) inits)
         | A.Location_global v2 ->
-           (match v with | A.I.V.Var x -> printf "NOTE1: Var %s\n" (A.I.V.pp_csym x) | A.I.V.Val (Constant.Symbolic s) -> addresses := StringSet.add (Constant.pp_symbol s) !addresses | _ -> ());
+           process_v v2;
            let initialiser = key_value_str (quote (A.I.V.pp_v v2)) (quote (A.I.V.pp_v v)) in
            (locations @ [initialiser], inits) in
-      let (locs, inits) = A.state_fold accum test.Test_herd.init_state ([], IntMap.empty)
+      let (locs, inits) = A.state_fold accum test.init_state ([], IntMap.empty)
       in (!addresses, locs, inits)
 
     let print_threads (test : T.result) (inits : string list IntMap.t) =
@@ -45,12 +48,12 @@ module Make
         print_endline "code = \"\"\"";
         List.iter (fun (_, instr) -> printf "\t%s\n" (A.dump_instruction instr)) code; (* TODO restore labels *)
         print_endline "\"\"\"" in
-      List.iter print_thread test.Test_herd.start_points
+      List.iter print_thread test.start_points
 
     let bracket = sprintf "(%s)"
 
     let rec format_constraint_expr = let open ConstrGen in function
-      | Atom atom -> begin match atom with (* dump_atom A.dump_location A.pp_location_brk (fun _ -> "###") A.I.FaultType.pp atom *)
+      | Atom atom -> begin match atom with
         | LV (loc, v) -> sprintf "%s = %s" (dump_rloc A.dump_location loc) (A.I.V.pp_v v)
         | LL (loc1, loc2) -> sprintf "%s = %s" (A.pp_location_brk loc1) (A.pp_location_brk loc2)
         | FF f -> Fault.pp_fatom A.I.V.pp_v A.I.FaultType.pp f end
@@ -73,8 +76,7 @@ module Make
       print_threads test inits;
       print_newline ();
       print_endline "[final]";
-      (* print_endline "TODO"; *)
-      let (expect, expr) = expect_and_expr test.Test_herd.cond in
+      let (expect, expr) = expect_and_expr test.cond in
       print_key "expect" (sprintf "\"%s\"" expect);
       print_key "assertion" (quote (format_constraint_expr expr))
   end
